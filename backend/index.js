2aa4696c52f32083120875e7e83a9c710607b7cf
Nebojsa Lukic <n.lukic@soccerbet.rs>
2024-02-22 17:10:13 +0100

    Implement POST /run route

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,43 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   return res.json({ message: "Hello World!" });
+});
+
+app.post("/run", (req, res, next) => {
+  const { language, code } = req.body;
+
+  if (!language) {
+    const error = new Error("Missing parameter: language");
+    error.status = 400;
+
+    return next(error);
+  }
+
+  if (!code) {
+    const error = new Error("Missing parameter: code");
+    error.status = 400;
+
+    return next(error);
+  }
+
+  //   TODO: Generate c++ file with content from the request
+  //   TODO: Run the file and send the response
+});
+
+app.use((err, req, res, next) => {
+  res.status = err.status || 500;
+
+  res.json({
+    error: {
+      message: err.message || "Error was occurred!",
+    },
+  });
 });
 
 app.listen(5000, () => {
