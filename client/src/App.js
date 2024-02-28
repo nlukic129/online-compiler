f567e910700a3c393fc68268ab6da1a6a690e810
Nebojsa Lukic <n.lukic@soccerbet.rs>
2024-02-28 20:12:13 +0100

    Implement polling on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [code, setCode] = useState("");
   const [language, setLanguage] = useState("cpp");
   const [output, setOutput] = useState("");
+  const [status, setStatus] = useState("");
+  const [jobId, setJobId] = useState("");
 
   const typeCodeHandler = (e) => {
     setCode(e.target.value);
@@ -16,8 +18,31 @@ function App() {
     const payload = { language, code };
 
     try {
+      setJobId("");
+      setStatus("");
+      setOutput("");
+
       const { data } = await axios.post("http://localhost:5000/run", payload);
-      setOutput(data.jobId);
+      setJobId(data.jobId);
+
+      let intervalId = setInterval(async () => {
+        const { data: dataRes } = await axios.get("http://localhost:5000/status", { params: { id: data.jobId } });
+        const { success, job, error } = dataRes;
+
+        if (!success) {
+          clearInterval(intervalId);
+          setStatus("Error: Please retry!");
+          return setOutput(error);
+        }
+
+        const { status: jobStatus, output: jobOutput } = job;
+        setStatus(jobStatus);
+
+        if (jobStatus === "pending") return;
+        setOutput(jobOutput);
+
+        clearInterval(intervalId);
+      }, 1000);
     } catch (err) {
       setOutput(err.response.data.error.message);
     }
@@ -41,6 +66,8 @@ function App() {
       <textarea cols="80" rows="20" value={code} onChange={typeCodeHandler}></textarea>
       <br />
       <button onClick={submitHandler}>Submit</button>
+      <p>{status}</p>
+      <p>{jobId && `JobID:${jobId}`}</p>
       <p>{output}</p>
     </div>
   );
